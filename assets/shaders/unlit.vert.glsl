52b80fe07435d49badc4dd4b7323e5232abd9e59
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2023-09-02 21:53:12 +0530

    Add some vertex attributes even though they are not used to get rid of validation Performance message

--- assets/shaders/unlit.vert.glsl
+++ assets/shaders/unlit.vert.glsl
@@ -1,6 +1,9 @@
 #version 450
 
 layout(location = 0) in vec3 InPos;
+layout(location = 1) in vec3 InNormal;
+layout(location = 2) in vec3 InColor;
+layout(location = 3) in vec3 InUV;
 
 layout(set = 0, binding = 0) uniform UniformBuffer
 {
