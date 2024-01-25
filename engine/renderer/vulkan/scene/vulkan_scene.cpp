73a36eca843d19dc44134d1948ca7173dc59b1fd
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2024-01-26 05:03:15 +0530

    replace push_back with emplace_back

--- engine/renderer/vulkan/scene/vulkan_scene.cpp
+++ engine/renderer/vulkan/scene/vulkan_scene.cpp
@@ -36,7 +36,7 @@ shoora_scene::AddCircleBody(const Shu::vec2f Pos, u32 ColorU32, f32 Radius, f32 
                             f32 InitialRotation)
 {
     shoora_body body{GetColor(ColorU32), Pos, Mass, Restitution, std::make_unique<shoora_shape_circle>(Radius), InitialRotation};
-    Bodies.push_back(std::move(body));
+    Bodies.emplace_back(std::move(body));
 }
 
 void
@@ -45,16 +45,16 @@ shoora_scene::AddBoxBody(const Shu::vec2f Pos, u32 ColorU32, f32 Width, f32 Heig
 {
     shoora_body body{GetColor(ColorU32), Pos, Mass, Restitution, std::make_unique<shoora_shape_box>(Width, Height),
                      InitialRotation};
-    Bodies.push_back(std::move(body));
+    Bodies.emplace_back(std::move(body));
 }
 
 void
 shoora_scene::AddPolygonBody(const u32 MeshId, const Shu::vec2f Pos, u32 ColorU32, f32 Mass, f32 Restitution,
                              f32 InitialRotation, f32 Scale)
 {
-    auto poly = std::make_unique<shoora_shape_polygon>(MeshId, Scale);
-    shoora_body body{GetColor(ColorU32), Pos, Mass, Restitution, std::move(poly), InitialRotation};
-    Bodies.push_back(std::move(body));
+    shoora_body body{GetColor(ColorU32), Pos, Mass, Restitution,
+                     std::make_unique<shoora_shape_polygon>(MeshId, Scale), InitialRotation};
+    Bodies.emplace_back(std::move(body));
 }
 
 void
