9691542518d5200cdaf0f21bac64e4d33bd0e32d
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2024-04-15 21:45:02 +0530

    adding more boxes onto the stack - still many instabilities. More contact points needed!

--- engine/renderer/vulkan/vulkan_renderer.cpp
+++ engine/renderer/vulkan/vulkan_renderer.cpp
@@ -744,16 +744,28 @@ InitScene()
 #endif
 
 #if 1
-    f32 startY = 1;
+    f32 startY = 5;
     auto *Body = Scene->AddCubeBody(shu::Vec3f(0, startY, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
     Body->FrictionCoeff = .5f;
     Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 1, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
     Body->FrictionCoeff = .5f;
-    // Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 2, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
-    // Body->FrictionCoeff = .5f;
-    // Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 3, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
-    // Body->FrictionCoeff = .5f;
-    
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 2, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 3, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 4, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 5, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 6, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 7, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 8, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+    Body = Scene->AddCubeBody(shu::Vec3f(0, startY + 9, 0), shu::Vec3f(1.0f), colorU32::White, 1.0f, .5f);
+    Body->FrictionCoeff = .5f;
+
     AddStandardSandBox();
 #endif
 
